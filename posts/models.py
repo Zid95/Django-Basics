9598c9a236a4bec71f8b9dea862aa148c36ac0a7
Zid95 <zid.abdulkarim@gmail.com>
2022-12-19 23:41:48 +0100

    relations

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=10000)
-    image = models.ImageField(upload_to='posts/', null=True)
+    image = models.ImageField(upload_to='posts/', default='default.png')
+    author = models.ForeignKey(
+        User, related_name='post_author', on_delete=models.SET_DEFAULT)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
