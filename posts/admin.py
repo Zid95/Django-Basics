2bf00c82af5e113e96b4c6f5577a82716dd9337e
Zid95 <zid.abdulkarim@gmail.com>
2022-12-23 16:33:08 +0100

    Create Form

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+
+
+admin.site.register(Post, PostAdmin)
